a901c89674e3798292ea0bd651935a26febbdfc1
aledguedes <alexandredguedes@gmail.com>
2026-01-29 09:47:27 -0300

    chore: enforce LF line endings

---
+++ src/app/pages/championships/championship-setup/setup-review/setup-review.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setup-review',
+  imports: [],
+  templateUrl: './setup-review.html',
+  styleUrl: './setup-review.css',
+})
+export class SetupReview {
+
+}
